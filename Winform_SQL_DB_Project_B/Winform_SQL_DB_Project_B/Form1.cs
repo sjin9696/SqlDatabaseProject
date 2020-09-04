217bf498962223e8176dc77241e1807e3b53192c
sjin9696 <sjin9696@gmail.com>
2020-09-04 14:20:47 +0900

    C#프로젝트 최초 생성

---
+++ Winform_SQL_DB_Project_B/Winform_SQL_DB_Project_B/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform_SQL_DB_Project_B
+{
+    public partial class index : Form
+    {
+        public index()
+        {
+            InitializeComponent();
+        }
+    }
+}
