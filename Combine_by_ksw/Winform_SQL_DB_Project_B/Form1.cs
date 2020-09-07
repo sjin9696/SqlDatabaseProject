d092f34c4212c1face6b8c313800e00c95fc2722
tkddn0109 <tkddn019n@gmail.com>
2020-09-08 00:29:59 +0900

    20200908
    코드정리 및 코드합체

---
+++ Combine_by_ksw/Winform_SQL_DB_Project_B/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform_SQL_DB_Project_B
+{
+    public partial class Form1 : Form
+    {
+        Class_DM DataManager = new Class_DM();
+        public Form1()
+        {
+            InitializeComponent();
+            Form1_TreeView(); //public partial class Form1 : Form
+            MessageBox.Show(DataManager.Connect_Oracle());
+            //dataGridView.DataSource = DataManager.ds.Tables[0];
+        }
+
+        private void button_Sql_exec_Click(object sender, EventArgs e) // 곽상우
+        {
+            DataManager.Cmd_Oracle(textBox_Right_Sql.Text);
+            dataGridView.DataSource = DataManager.ds.Tables[0];
+        }
+
+        private void button_Select_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Insert_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Updata_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Delete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void treeView_Left_AfterSelect(object sender, TreeViewEventArgs e) //허선용 -> 곽상우
+        {
+            if (e.Node.Text == "테이블" || e.Node.Text == "프로시저" || e.Node.Text == "시퀀스")
+            {
+
+            }
+            else
+            {
+                textBox_Middle.Text = null;
+                string notice = null;
+                notice = DataManager.Tree_Oracle(e.Node.Text, e.Node.Parent.Text);
+                dataGridView.DataSource = DataManager.ds.Tables[0];
+                textBox_Middle.Text = notice;
+            }
+        }
+    }
+}
