a16b2ff352de29a40ec1445c9ec000a94777a38e
sjin9696 <sjin9696@gmail.com>
2020-09-09 14:23:04 +0900

    파일변경 테스트

--- Winform_SQL_DB_Project_B - 종훈/Winform_SQL_DB_Project_B/Form1_TreeView.cs
+++ Winform_SQL_DB_Project_B - 종훈/Winform_SQL_DB_Project_B/Form1_TreeView.cs
@@ -13,21 +13,25 @@ namespace Winform_SQL_DB_Project_B
         public void Form1_TreeView()
         {
             int length = 5;
-            TreeNode table = new TreeNode("Table", 0, 0);
+            TreeNode TAble = new TreeNode("Table", 0, 0);
             for (int i = 1; i <= length; i++)
-                table.Nodes.Add("테이블" + i, i + "번테이블", 0, 0);
+                TAble.Nodes.Add("테이블" + i, i + "번테이블", 0, 0);
 
             TreeNode proseger = new TreeNode("프로시저", 1, 1);
             for (int i = 1; i <= length; i++)
                 proseger.Nodes.Add("프로시저" + i, i + "번프로시저", 1, 1);
 
             //트리노드를 트리뷰에 적용한다.
-            treeView_Left.Nodes.Add(table);
+            treeView_Left.Nodes.Add(TAble);
             treeView_Left.Nodes.Add(proseger);
 
             //모든 트리 노드를 보여준다.
             treeView_Left.ExpandAll();
+            treeView_Left.ExpandAll();
         }
+        public void test()
+        {
 
+        }
     }
 }
